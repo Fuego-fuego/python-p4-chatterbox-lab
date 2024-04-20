8f4645b13df585e53e9c68d9588ee9e62f79b18d
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-04-20 20:05:25 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+from sqlalchemy import asc
 from models import db, Message
 
 app = Flask(__name__)
@@ -13,14 +13,75 @@ CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
+@app.route('/')
+def index():
+    return "<h1>Up and running</h1>"
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method=='GET':
+        messages = Message.query.order_by(asc(Message.created_at)).all()
+        messages_serialized = [ message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            messages_serialized,
+            200
+        )
+        
+        return response
+    elif request.method=='POST':
+        data = request.get_json()
+        
+        new_message = Message(
+            body = data['body'],
+            username=data['username']       
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        new_message_dict = new_message.to_dict()
+        
+        response=make_response(
+            new_message_dict,
+            201
+        )
+        
+        return response
+        
+
+@app.route('/messages/<int:id>',methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method=='PATCH':
+        for attr in request.get_json():
+            data= request.get_json()
+            setattr(message, attr,data[attr])
+            
+        db.session.add(message)
+        db.session.commit()
+        
+        message_dict = message.to_dict()
+
+        response = make_response(
+                message_dict,
+                200
+            )
+        
+        return response
+        
+    elif request.method=='DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        
+        response_body = {
+            'delete_successful':True,
+            "message": "Review deleted."
+        }
+        
+        
+        
+        return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555)
